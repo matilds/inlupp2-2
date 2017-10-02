c604fb7ac4a49212edda440bd4f6114249a3bcb6
carle3 <cacaca@live.se>
2017-10-02 19:14:25 +0200

    tree_insert?

--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "tree.h"
+#include <stdio.h> 
 
 typedef struct node node_t;
 
@@ -98,6 +99,19 @@ int node_depth(node_t *node)
 {
   int left_depth = 0;
   int right_depth = 0;
+  if(node == NULL)
+    {
+       if(left_depth >= right_depth)
+         {
+           return left_depth;
+             
+         }
+       else
+         {
+           return right_depth;
+         }
+        
+    }
   if(node->left != NULL)
     {
     left_depth = node_depth(node->left) + 1;
@@ -126,28 +140,28 @@ int tree_depth(tree_t *tree)
 
 node_t **place_to_insert_node(node_t *node, tree_key_t key)
 {
-  node_t **pekare = &node;
-  if (key.i == (*pekare)->key.i)
+
+  
+  if(key.i > node->key.i)
     {
-      return NULL;
-    }
-  if ((*pekare) == NULL)
-  {
-    return pekare;
-  }
-    
-  if (key.i < (*pekare)->key.i)
-    {
-      pekare = &(*pekare)->left;
-      place_to_insert_node(*pekare, key);
-    }
-  if (key.i > (*pekare)->key.i)
-    {
-      pekare = &(*pekare)->right;
-      place_to_insert_node(*pekare, key);
+      if (node->right == NULL)
+        {
+          return &(node->right);
+        }
+      return place_to_insert_node(node->right , key);
+     
     }
 
-  return pekare;
+  if(key.i < node->key.i)
+    {
+      
+      if(node->left == NULL)
+        {
+          return &(node->left);
+        }
+      return place_to_insert_node(node->left , key);
+    }
+  return NULL;
 }
 
 node_t **place_to_insert_tree(tree_t *tree, tree_key_t key)
@@ -155,36 +169,36 @@ node_t **place_to_insert_tree(tree_t *tree, tree_key_t key)
   return place_to_insert_node(tree->root, key);
 }
 
-node_t *left_left (node_t *node)
+node_t **left_left (node_t **node)
 {
-  node_t *tmp = node->left->right;
-  node->left->right = node;
-  node = node->left;
-  node->right->left = tmp;
+  node_t *tmp = (*node)->left->right;
+  (*node)->left->right = (*node);
+  (*node) = (*node)->left;
+  (*node)->right->left = tmp;
   return node;
 }
-node_t *left_right(node_t *node)
+node_t **left_right(node_t **node)
 {
-   node->left->right->left = node->left;
-   node->left = node->left->right;
-   node->left->right = NULL;
+   (*node)->left->right->left = (*node)->left;
+   (*node)->left = (*node)->left->right;
+   (*node)->left->right = NULL;
    return left_left(node);
 }
 
-node_t *right_right(node_t *node)
+node_t **right_right(node_t **node)
 {
-  node_t *tmp = node->right->left;
-  node->left->left = node;
-  node = node->right;
-  node->left->right = tmp;
+  node_t *tmp = (*node)->right->left;
+  (*node)->left->left = (*node);
+  (*node) = (*node)->right;
+  (*node)->left->right = tmp;
    return node;
 }
 
-node_t *right_left(node_t *node)
+node_t **right_left(node_t **node)
 {
-   node->right->left->right = node->right;
-   node->right = node->right->left;
-   node->right->left = NULL;
+   (*node)->right->left->right = (*node)->right;
+   (*node)->right = (*node)->right->left;
+   (*node)->right->left = NULL;
    return right_right(node);
 }
 
@@ -232,53 +246,53 @@ node_t *balance_node(node_t *node)
 }
 */
 
-node_t *find_unbalanced_to_fix(node_t *node , node_t *fel)
+node_t **find_unbalanced_to_fix(node_t **node , node_t *unbalanced_part)
 {
-  int depth_right = node_depth(node->right);
-  int depth_left = node_depth(node->left);
+  int depth_right = node_depth((*node)->right);
+  int depth_left = node_depth((*node)->left);
   
   if (node == NULL)
     {
-      return fel;
+      return &unbalanced_part;
     }
-   if( abs( depth_left -depth_right) > 1)
+  if( abs( depth_left -depth_right) > 1)
     {
-      fel = node;
+      unbalanced_part = *node;
       if(depth_left > depth_right)
         {
-          find_unbalanced_to_fix(node->left, fel);
+          find_unbalanced_to_fix(&(*node)->left, unbalanced_part);
         }
       else
         {
-          find_unbalanced_to_fix(node->right, fel);
+          find_unbalanced_to_fix(&(*node)->right, unbalanced_part);
         }
     }
-   return fel;
+  return &unbalanced_part->
 }
 
-node_t *balanced_node(node_t *node)
+node_t **balanced_node(node_t *node)
 {
   
   
   if (node_depth(node->right) > node_depth(node->left))
-    {
+    {  
       if (node_depth(node->right->right) > node_depth(node->right->left))
         {
-          return right_right(node);
+          return right_right(&node);
         }
       else if (node_depth(node->right->left) > node_depth(node->right->right) && node->left == NULL)
         {
-          return right_left(node);
+          return right_left(&node);
         }
       else if( node_depth(node->right->left->right) > node_depth(node->right->left->left))
         {
-          left_right(node->right);
-          return right_right(node);
+          left_right(&node->right);
+          return right_right(&node);
         }
       else
         {
-          left_left(node->right);
-          return right_right(node);
+          left_left(&node->right);
+          return right_right(&node);
         }
       
     }
@@ -291,21 +305,21 @@ node_t *balanced_node(node_t *node)
     {
       if (node_depth(node->left->left) > node_depth(node->left->right))
         {
-          return left_left(node);
+          return left_left(&node);
         }
       else if (node_depth(node->left->right) > node_depth(node->left->left) && node->right == NULL)
         {
-          return left_right(node);
+          return left_right(&node);
         }
       else if( node_depth(node->left->right->left) > node_depth(node->left->right->right))
         {
-          right_left(node->left);
-          return left_left(node);
+          right_left(&node->left);
+          return left_left(&node);
         }
       else
         {
-          right_right(node->left);
-          return left_left(node);
+          right_right(&node->left);
+          return left_left(&node);
         }
     }
     
@@ -321,7 +335,19 @@ bool tree_insert(tree_t *tree, tree_key_t key, elem_t elem)
     {
       elem = copy_fun(elem);
     }
+  //test
+  if(tree->root == NULL)
+    {
+      node_t *new = calloc(1,sizeof(node_t));
+      new->elem = elem;
+      new->key = key;
+      tree->root = new;
+      return true;
+    }
+  //
+  
   node_t **insert_place = place_to_insert_tree(tree, key);
+  // if key exists in tree
   if(insert_place == NULL)
     {
       return false;
@@ -332,24 +358,26 @@ bool tree_insert(tree_t *tree, tree_key_t key, elem_t elem)
       new_node->elem = elem;
       new_node->key = key;
       *insert_place = new_node;
+      
       if(abs(node_depth(tree->root->right) - node_depth(tree->root->left)) <= 1)
         {
           return true;
         }
       else
         {
-      node_t *fel = calloc(1,sizeof(node_t));
-      do
+          node_t *unbalanced_part = calloc(1,sizeof(node_t));/* TODO:  *//* TODO:  *//* TODO:  */
+          do
         {
-          fel = NULL;
-          find_unbalanced_to_fix(tree->root , fel);
-          balanced_node(fel);
+          unbalanced_part = NULL; //set to NULL every iteration.
+          node_t ** u_p = find_unbalanced_to_fix(tree->root , unbalanced_part);
+          balanced_node(unbalanced_part);
       
         }
-      while(fel != NULL);
-        
+      while(unbalanced_part != NULL);
+      
         }
     }
+  free(insert_place);
   return true;
 }
 
@@ -399,5 +427,94 @@ bool tree_get(tree_t *tree, tree_key_t key, elem_t *result)
       *result = node_result->elem;
       return true;
     }
+}
+
+
+//rekursion vs iteration
+void print_num_iteration(int num)
+{
+  int x = 1;
+  while (x <= num)
+    {
+      printf("%d\n" , x);
+      x++;
+    }
+}
+
+void print_num_rekursion(int num , int current_num)
+{
+  if( current_num <= num)
+    {
+      printf("%d\n" , current_num);
+      print_num_rekursion(num , current_num + 1);
+    }
+  else
+    {
+      return;
+    }
+  
+}
+
+void ekvivalenta()
+{
+  int b = 1;
+  int a = 1;
+  if ( &a == &b) printf("a är ekvivalent med b\n");
+  else puts("a är inte ekvivalent med b");
+  if( a == b) puts("a och b är identiska");
+  else puts("a är inte identisk med b");
+}
+
+
+void identical()
+{
+  int b = 1;
+  int *a = &b;
+  if ( a == &b) printf("a är ekvivalent med b\n");
+  else puts("a är inte ekvivalent med b");
+  if( *a == b) puts("a och b är identiska");
+  else puts("a är inte identisk med b");
+}
+
+
+int main()
+{
+  /*
+  puts("iteration");
+  print_num_iteration(5);
+  puts("rekursion");
+  print_num_rekursion(5,1);
+  puts("\nidentiska?");
+  identical();
+  puts("\nekvivalenta?");
+  ekvivalenta();
+  */
+  tree_t *tree  = tree_new(NULL,NULL,NULL,NULL);
+  elem_t key;
+  key.i = 3;
+  elem_t elem;
+  elem.i = 555;
+  tree_insert(tree, key , elem);
+  elem_t key1;
+  key1.i = 2;
+  elem_t elem1;
+  elem.i = 555;
+
+  tree_insert(tree , key1 , elem1);
+
+   int i = tree_depth(tree);
+  int j = tree_size(tree);
+  printf("depth = %d , size = %d\n" , i ,j);
+   elem_t key2;
+  key2.i = 1;
+  elem_t elem2;
+  elem2.i = 5552;
+
+  tree_insert(tree , key2 , elem2);
+
+ 
+  printf("root = %d\n" , tree->root->key.i);
+  printf("root->left = %d\n" , tree->root->left->key.i);
+  // printf("root->left->left = %d\n" , tree->root->left->right->key.i);
 }
 
